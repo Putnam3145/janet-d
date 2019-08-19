1e173a4f5d532e5d8aef598b45e0dae9296d1cd1
Putnam <putnam3145@gmail.com>
2019-08-18 23:44:36 -0700

    Added more proper test

--- source/tests/test.d
+++ source/tests/test.d
@@ -4,13 +4,20 @@ unittest
 {
     import std.file;
     import std.stdio;
-    janet_init;
-    pragma(msg,__traits(getLinkage,janet_core_env));
-    Janet testJanet;
-    const ubyte[] file = cast(const(ubyte[]))read("./source/tests/hello.janet");
-    const(ubyte)* realFile = cast(const(ubyte)*)file;
-    int realFileLength = cast(int)(file.length);
-    auto empty = janet_table(1);
-    auto env = janet_core_env(empty);
-    auto n = janet_dobytes(env,realFile,realFileLength,cast(const(char)*)"./source/tests/hello.janet",&testJanet);
+    import std.parallelism;
+    // run the examples in parallel
+    foreach(DirEntry entry;parallel(dirEntries("./source/tests/","*.janet",SpanMode.depth),1))
+    {
+        janet_init();
+        Janet testJanet;
+        const ubyte[] file = cast(const(ubyte[]))read(entry.name);
+        writeln("Testing file ",entry.name);
+        const(ubyte)* realFile = cast(const(ubyte)*)file;
+        int realFileLength = cast(int)(file.length);
+        auto empty = janet_table(1);
+        auto env = janet_core_env(empty);
+        auto errorString = entry.name~" errored!";
+        assert(janet_dobytes(env,realFile,realFileLength,cast(const(char)*)(entry.name),&testJanet)==0,errorString);
+        writeln("Success!");
+    }
 }
