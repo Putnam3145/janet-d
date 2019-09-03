d1f0cae78fc2e27078cb4ddbaef24e6b0d2138ea
Putnam <putnam3145@gmail.com>
2019-09-02 19:36:12 -0700

    Added some more helper funcs

--- source/all.d
+++ source/all.d
@@ -2,6 +2,51 @@ module janet.all;
 
 public import janet.d;
 
+public import janet.c : JanetFunction,JanetTable,JanetKV;
+
+import janet.c;
+
+/**
+    Define an immutable value in Janet.
+    Immutable means something slightly different in Janet than in D,
+    so this doesn't take in a const or immutable value.
+*/
+void janetDef(JanetTable* env,string name,Janet val,string documentation = "")
+{
+    import std.string : toStringz;
+    import janet.c : janet_def;
+    janet_def(env,toStringz(name),val,toStringz(documentation));
+}
+/**
+    Define a mutable value in Janet.
+*/
+void janetVar(JanetTable* env,string name,Janet val,string documentation = "")
+{
+    import std.string : toStringz;
+    import janet.c : janet_var;
+    janet_var(env,toStringz(name),val,toStringz(documentation));
+}
+
+Janet get(JanetTable* tbl,string key)
+{
+    return janet_table_get(tbl,janetWrap(key));
+}
+
+Janet get(JanetTable* tbl,Janet key)
+{
+    return janet_table_get(tbl,key);
+}
+
+Janet get(JanetKV* tbl,string key)
+{
+    return janet_struct_get(tbl,janetWrap(key));
+}
+
+Janet get(JanetKV* tbl,Janet key)
+{
+    return janet_struct_get(tbl,key);
+}
+
 unittest
 {
     import std.file;
